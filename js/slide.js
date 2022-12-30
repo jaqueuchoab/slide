ec8855263cc5ce17f5514a070d92966c34ab5904
jaqueuchoab <jaqueline.uchoab@gmail.com>
2022-12-30 17:04:28 -0300

    distancias atualizadas

--- js/slide.js
+++ js/slide.js
@@ -2,23 +2,44 @@ export default class Slide {
   constructor(slideItem, slideWrapper) {
     this.slideItem = document.querySelector(slideItem);
     this.slideWrapper = document.querySelector(slideWrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0
+    }
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slideItem.style.transform = `translate3d(${distX}px, 0px, 0px)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    console.log(this.dist.movement);
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
+    // O clientX é uma propriedade de leitura da interface MouseEvent que fornece as coordenadas horizontais dentro da área do aplicativo
+    this.dist.startX = event.clientX;
     this.slideWrapper.addEventListener('mousemove', this.onMove);
   }
 
   onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
     this.slideWrapper.removeEventListener('mousemove', this.onMove)
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addEventSlide() {
     this.slideWrapper.addEventListener('mousedown', this.onStart);
-    this.slideWrapper.addEventListener('mouseup', this.onEnd);
+    this.slideWrapper.addEventListener('mouseleave', this.onEnd);
   }
 
   bindEvents() {
